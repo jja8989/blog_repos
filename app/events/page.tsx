7fca5c5bd35795ab22da07436a178978e1865cdd
holinessnine <seg3981@gmail.com>
2024-12-11 17:02:29 +0900

    Event page 수정

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -11,6 +11,18 @@ export default function Events() {
       </section>
 
       {/* Add event-related content here */}
+      {/* 구글 캘린더 임베드 */}
+      <div className="flex justify-center">
+        <iframe
+          src="https://calendar.google.com/calendar/embed?src=ksofg%40snu.ac.kr&ctz=Asia%2FSeoul" // 여기에 복사한 구글 캘린더 임베드 링크를 삽입
+          style={{ border: "0" }}
+          width="800"
+          height="600"
+          frameBorder="0"
+          scrolling="no"
+        ></iframe>
+      </div>
+    
     </Layout>
   );
 }
